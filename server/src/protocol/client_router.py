cd36816dec921f7d406f07e569eb35bcb91dba1d
89mpxf <diamond061@outlook.com>
2022-10-28 10:46:31 -0400

    Added direct YouTube route and added error reports

--- server/src/protocol/client_router.py
+++ server/src/protocol/client_router.py
@@ -3,13 +3,22 @@ import json
 
 # Import local dependencies
 from ..services.youtube import YoutubeServiceHandler
+from ..util import logTime
 
 class ClientRouter(object):
     def __init__(self):
         self.YoutubeServiceHandler = YoutubeServiceHandler()
 
     def route(self, data):
-        if data["service"] == "youtube":
-            if data["operation"] == "search":
-                output = self.YoutubeServiceHandler.search(data["payload"]["query"], data["payload"]["amount"])
-                return {"success": True, "response": output}
+        try:
+            if data["service"] == "youtube":
+                if data["operation"] == "search":
+                    output = self.YoutubeServiceHandler.search(data["payload"]["query"], data["payload"]["amount"])
+                    return {"success": True, "response": output}
+                elif data["operation"] == "get":
+                    output = self.YoutubeServiceHandler.get(data["payload"]["url"])
+                    return {"success": True, "response": output}
+        except Exception as e:
+            print(logTime() + f"An error occured while processing request: {type(e).__name__} - {e} ({e.__traceback__.tb_next.tb_frame.f_code.co_filename}@{e.__traceback__.tb_next.tb_frame.f_lineno})")
+            return {"success": False, "error": "An error occured while handling this request."}
+
--- server/src/services/youtube.py
+++ server/src/services/youtube.py
@@ -16,7 +16,7 @@ class YoutubeServiceHandler(object):
             }
         }
 
-    def search(self, query, amount: int = 10):
+    def search(self, query: str, amount: int = 10):
         search_results = []
         with YoutubeDL(self.YDL_OPTIONS["search"]) as api:
             print(logTime() + f"Executing YouTube search with query '{query}'...")
@@ -36,3 +36,33 @@ class YoutubeServiceHandler(object):
                 search_results.append(data)
                 
         return search_results
+
+    def get(self, url: str):
+        results = []
+        with YoutubeDL(self.YDL_OPTIONS["search"]) as api:
+            print(logTime() + f"Getting YouTube data from '{url}'...")
+            st = time.time()
+            raw_data = api.extract_info(url, download=False)
+            et = time.time()
+            print(logTime() + f"YouTube data fetch for '{url}' completed ({str(round(et - st, 2))}s)")
+            if not "entries" in raw_data:
+                data = {}
+                data["id"] = raw_data["id"]
+                data["title"] = raw_data["title"]
+                data["artist"] = raw_data["uploader"]
+                data["duration"] = raw_data["duration"]
+                data["cover"] = raw_data["thumbnail"]
+                data["source"] = raw_data["formats"][0]["url"]
+                results.append(data)
+            else:
+                for i in range(len(raw_data["entries"])):
+                    print(raw_data["entries"][i])
+                    data = {}
+                    data["id"] = raw_data["entries"][i]["id"]
+                    data["title"] = raw_data["entries"][i]["title"]
+                    data["artist"] = raw_data["entries"][i]["uploader"]
+                    data["duration"] = raw_data["entries"][i]["duration"]
+                    data["cover"] = raw_data["entries"][i]["thumbnail"]
+                    data["source"] = raw_data["entries"][i]["formats"][0]["url"]
+                    results.append(data)
+            return results
