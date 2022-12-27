d99b636ab8a6910c5dc2d763d7f891b371ae63ce
89mpxf <diamond061@outlook.com>
2022-12-27 13:33:13 -0500

    Fixed stdlib import overwrite error

--- client/FlexMusic/util/__init__.py
+++ client/FlexMusic/util/__init__.py
@@ -3,4 +3,4 @@ FlexMusic Client Utility Library\n
 This library includes utilities that can be used alongside the FlexMusic Client to make deployment easier.
 '''
 
-from .queue import Queue
+from ._queue import Queue
