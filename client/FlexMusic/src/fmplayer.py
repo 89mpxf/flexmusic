41a1cec4d3873d1c4b5b88f91cca1775ea22614e
89mpxf <diamond061@outlook.com>
2022-12-27 14:37:36 -0500

    Added overwrite for play method

--- client/FlexMusic/src/fmplayer.py
+++ client/FlexMusic/src/fmplayer.py
@@ -1,13 +1,24 @@
 # Import dependencies
-import discord
+from discord import VoiceClient, AudioSource
+from typing import Callable, Optional, Any
 
 # Import local dependencies
 from .track import Track
 from ..util._queue import Queue
 
-class FMPlayer(discord.VoiceClient):
-    def __init__(self, voice_client: discord.VoiceClient):
+class FMPlayer(VoiceClient):
+    def __init__(self, voice_client: VoiceClient):
         self.__class__ = type(voice_client.__class__.__name__, (self.__class__, voice_client.__class__), {})
         self.__dict__ = voice_client.__dict__
         self._voice_client = voice_client
         self.queue = Queue()
+
+    def play(self, track: Track | AudioSource, *, after: Callable[[Optional[Exception]], Any] = None):
+        """
+        This is the play method for the FMPlayer class. It wraps the play method of the VoiceClient class, so it can still be used as normal.\n
+        This method adds support for FlexMusic Track objects.
+        """
+        if isinstance(track, Track):
+            self._voice_client.play(track.src, after=after)
+        else:
+            self._voice_client.play(track, after=after)
