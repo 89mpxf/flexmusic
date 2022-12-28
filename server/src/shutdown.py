790b5d7e3f47c52944556f9524fa2d8ecf148e5e
89mpxf <diamond061@outlook.com>
2022-12-28 01:08:41 -0500

    Fixed punctuation mismatch

--- server/src/shutdown.py
+++ server/src/shutdown.py
@@ -6,11 +6,11 @@ from .session import SessionManager
 from .util import logTime
 
 def shutdown(session_manager: SessionManager):
-    print(logTime() + "Received server shutdown signal.")
+    print(logTime() + "Received server shutdown signal")
     for session in session_manager.sessions:
         print(logTime() + f"Closing connection to {session.addr[0]}:{session.addr[1]}...")
         session.close()
 
 def close_server(server: socket):
-    print(logTime() + "Server shutdown successfully.")
+    print(logTime() + "Server shutdown successfully")
     server.close()
