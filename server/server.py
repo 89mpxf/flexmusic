f6a625a5d20bfbda5036ee5c370c9ee20155d9ac
89mpxf <diamond061@outlook.com>
2022-12-28 00:45:01 -0500

    Move server initialization functions to separate file

--- server/server.py
+++ server/server.py
@@ -1,28 +1,4 @@
-# Import dependencies
-from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
-
-# Import local dependencies
-from src.motd import splash
-from src.session import SessionManager, session_bootstrapper
-
-def start_server(host: str = "0.0.0.0", port: int = 5000) -> socket:
-    server = socket(AF_INET, SOCK_STREAM)
-    server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    server.bind((host, port))
-    splash(host, port)
-    return server
-
-def run_server(server):
-    session_manager = SessionManager()
-    try:
-        while True:
-            server.listen(5)
-            sock, addr = server.accept()
-            session_bootstrapper(sock, addr, session_manager)
-    except KeyboardInterrupt:
-        pass
-    except:
-        raise
+from src.server import bootstrap
 
 if __name__ == '__main__':
-    run_server(start_server())
+    bootstrap()
---
+++ server/src/server.py
@@ -0,0 +1,28 @@
+# Import dependencies
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+
+# Import local dependencies
+from src.session import SessionManager, session_bootstrapper
+from src.motd import splash
+
+def start_server(host: str = "0.0.0.0", port: int = 5000) -> socket:
+    server = socket(AF_INET, SOCK_STREAM)
+    server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    server.bind((host, port))
+    splash(host, port)
+    return server
+
+def runtime(server: socket):
+    session_manager = SessionManager()
+    try:
+        while True:
+            server.listen(5)
+            sock, addr = server.accept()
+            session_bootstrapper(sock, addr, session_manager)
+    except KeyboardInterrupt:
+        pass
+    except:
+        raise
+
+def bootstrap():
+    runtime(start_server())
